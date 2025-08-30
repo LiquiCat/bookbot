470598781a57779e1c2933473eca5127861afc11
LiquiCat <arseniy.priymakov@gmail.com>
2025-08-31 01:55:05 +0400

    added word count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from stats import get_word_count
+from stats import get_word_count, get_letter_count
 
 def get_book_text(file_path: str) -> str:
     with open(file_path) as file:
@@ -13,7 +13,8 @@ def main():
     book_path = "./books/frankenstein.txt"
     book_text = get_book_text(book_path)
     word_count = get_word_count(book_text)
-    print(word_count)
+    letter_count = get_letter_count(book_text)
+    print(letter_count)
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,2 +1,17 @@
+from typing import Dict
+
 def get_word_count(whole_book: str) -> str:
     return f"{len(whole_book.split())} words found in the document"
+
+def get_letter_count(whole_book: str) -> Dict[str, int]:
+    
+    count_words = {}
+
+    for char in whole_book:
+        c = char.lower()
+        if c in count_words:
+            count_words[c] += 1
+        else:
+            count_words[c] = 1
+
+    return count_words
